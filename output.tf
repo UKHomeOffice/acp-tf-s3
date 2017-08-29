59ea32c5e6ff19eb59d11918733889ddc9687c90
Jay Keshur <jay.keshur@digital.homeoffice.gov.uk>
2017-08-29 10:55:44 +0100

    Outputs for s3 / kms

--- output.tf
+++ output.tf
@@ -1,19 +1,9 @@
+output "s3_bucket_id" {
+  description = "The S3 bucket ID we just created"
+  value       = "${aws_s3_bucket.s3_bucket.id}"
+}
 
-#output "s3_bucket_id" {
-#  description = "The S3 bucket ID we just created"
-#  value       = "${aws_s3_bucket.s3_bucket.id}"
-#}
-#
-#output "s3_bucket_arn" {
-#  description = "The S3 bucket amazon resource name"
-#  value       = "${aws_s3_bucket.s3_bucket.arn}"
-#}
-#
-#output "s3_bucket_domain_name" {
-#  description = "The S3 bucket doomain name"
-#  value       = "${aws_s3_bucket.s3_bucket.bucket_domain_name}"
-#}
-#output "s3_bucket_hosted_zone_id" {
-#  description = "The S3 bucket host zone id"
-#  value       = "${aws_s3_bucket.s3_bucket.hosted_zone_id}"
-#} 
+output "s3_bucket_kms_key" {
+  description = "The KMS ID used for the bucket"
+  value       = "${aws_kms_key.s3_bucket_kms_key.key_id}"
+}
