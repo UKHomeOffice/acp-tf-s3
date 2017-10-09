5ae24d45d5a901b38d1a65bd7b94ec6301ef8372
Kashif Saadat <kashifsaadat@gmail.com>
2017-10-09 14:58:44 +0100

    Make KMS encryption optional

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ data "aws_region" "current" {
 resource "aws_kms_key" "s3_bucket_kms_key" {
   count = "${var.kms_alias == "" ? 0 : 1}"
 
-  description = "A kms key for encrypting/decryting S3 bucket ${var.name}"
+  description = "A kms key for encrypting/decrypting S3 bucket ${var.name}"
   policy      = "${data.aws_iam_policy_document.kms_key_policy_document.json}"
 
   tags = "${merge(var.tags, map("Name", format("%s-%s", var.environment, var.name)), map("Env", var.environment), map("KubernetesCluster",var.environment))}"
@@ -33,7 +33,7 @@ resource "aws_s3_bucket" "s3_bucket" {
     id      = "transition-to-infrequent-access-storage"
     enabled = "${var.lifecycle_infrequent_storage_transition_enabled}"
 
-    prefix  = "${var.lifecycle_infrequent_storage_object_prefix}"
+    prefix = "${var.lifecycle_infrequent_storage_object_prefix}"
 
     transition {
       days          = "${var.lifecycle_days_to_infrequent_storage_transition}"
@@ -45,7 +45,7 @@ resource "aws_s3_bucket" "s3_bucket" {
     id      = "transition-to-glacier"
     enabled = "${var.lifecycle_glacier_transition_enabled}"
 
-    prefix  = "${var.lifecycle_glacier_object_prefix}"
+    prefix = "${var.lifecycle_glacier_object_prefix}"
 
     transition {
       days          = "${var.lifecycle_days_to_glacier_transition}"
@@ -57,7 +57,7 @@ resource "aws_s3_bucket" "s3_bucket" {
     id      = "expire-objects"
     enabled = "${var.lifecycle_expiration_enabled}"
 
-    prefix  = "${var.lifecycle_expiration_object_prefix}"
+    prefix = "${var.lifecycle_expiration_object_prefix}"
 
     expiration {
       days = "${var.lifecycle_days_to_expiration}"
@@ -89,6 +89,8 @@ resource "aws_iam_user_policy" "s3_bucket_user_policy" {
 }
 
 data "aws_iam_policy_document" "s3_bucket_with_kms_policy_document" {
+  count = "${var.kms_alias == "" ? 0 : 1}"
+
   policy_id = "${var.bucket_iam_user}Policy"
 
   statement {
