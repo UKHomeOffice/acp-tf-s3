433104bf56259a6f8a51e5b8cfd2c73c74d631e8
Ben Eustace <ben.eustace@digital.homeoffice.gov.uk>
2017-10-23 21:58:16 +0100

    ip white listing iam policy

---
+++ policy.tf
@@ -0,0 +1,347 @@
+data "aws_iam_policy_document" "s3_bucket_with_kms_policy_document_1" {
+  policy_id = "${var.bucket_iam_user}S3BucketPolicy"
+
+  statement {
+    sid    = "IAMS3BucketPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}",
+    ]
+
+    actions = [
+      "s3:ListBucket",
+    ]
+  }
+
+  statement {
+    sid    = "IAMS3ObjectPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}/*",
+    ]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "s3_bucket_with_kms_policy_document_2" {
+  policy_id = "${var.bucket_iam_user}KMSPolicy"
+
+  statement {
+    sid    = "KMSPermissions"
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:alias/${var.kms_alias}",
+    ]
+
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+      "kms:Encrypt",
+      "kms:GenerateDataKey",
+      "kms:GenerateDataKeyWithoutPlaintext",
+      "kms:GenerateRandom",
+      "kms:GetKeyPolicy",
+      "kms:GetKeyRotationStatus",
+      "kms:ReEncrypt",
+    ]
+  }
+
+  statement {
+    sid    = "DenyCondition"
+    effect = "Deny"
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}/*",
+    ]
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = ["aws:kms"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "s3_bucket_policy_document" {
+  policy_id = "${var.bucket_iam_user}S3BucketPolicy"
+
+  statement {
+    sid    = "IAMS3BucketPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}",
+    ]
+
+    actions = [
+      "s3:ListBucket",
+    ]
+  }
+
+  statement {
+    sid    = "IAMS3ObjectPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}/*",
+    ]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "kms_key_policy_document" {
+  policy_id = "${var.kms_alias}KMSPolicy"
+
+  statement {
+    sid    = "IAMPermissions"
+    effect = "Allow"
+
+    resources = ["*"]
+
+    actions = [
+      "kms:*",
+    ]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+      ]
+    }
+  }
+
+  statement {
+    sid    = "KeyAdministratorsPermissions"
+    effect = "Allow"
+
+    resources = ["*"]
+
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "s3_bucket_with_kms_policy_document_whitelist_1" {
+  policy_id = "${var.bucket_iam_user}S3BucketPolicy"
+
+  statement {
+    sid    = "IAMS3BucketPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}",
+    ]
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values   = ["${var.whitelist_ip}"]
+    }
+  }
+
+  statement {
+    sid    = "IAMS3ObjectPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}/*",
+    ]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+    ]
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values   = ["${var.whitelist_ip}"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "s3_bucket_with_kms_policy_document_whitelist_2" {
+  policy_id = "${var.bucket_iam_user}KMSPolicy"
+
+  statement {
+    sid    = "KMSPermissions"
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:alias/${var.kms_alias}",
+    ]
+
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+      "kms:Encrypt",
+      "kms:GenerateDataKey",
+      "kms:GenerateDataKeyWithoutPlaintext",
+      "kms:GenerateRandom",
+      "kms:GetKeyPolicy",
+      "kms:GetKeyRotationStatus",
+      "kms:ReEncrypt",
+    ]
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values   = ["${var.whitelist_ip}"]
+    }
+  }
+
+  statement {
+    sid    = "DenyCondition"
+    effect = "Deny"
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}/*",
+    ]
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = ["aws:kms"]
+    }
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values   = ["${var.whitelist_ip}"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "s3_bucket_policy_document_whitelist" {
+  policy_id = "${var.bucket_iam_user}S3BucketPolicy"
+
+  statement {
+    sid    = "IAMS3BucketPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}",
+    ]
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values   = ["${var.whitelist_ip}"]
+    }
+  }
+
+  statement {
+    sid    = "IAMS3ObjectPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}/*",
+    ]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+    ]
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values   = ["${var.whitelist_ip}"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "kms_key_policy_document_whitelist" {
+  policy_id = "${var.kms_alias}KMSPolicy"
+
+  statement {
+    sid    = "IAMPermissions"
+    effect = "Allow"
+
+    resources = ["*"]
+
+    actions = [
+      "kms:*",
+    ]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+      ]
+    }
+  }
+
+  statement {
+    sid    = "KeyAdministratorsPermissions"
+    effect = "Allow"
+
+    resources = ["*"]
+
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion",
+    ]
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values   = ["${var.whitelist_ip}"]
+    }
+  }
+}
