91d48bc81ddc97dd15f6d0cb2b57bc5b4aa07241
Ben Eustace <ben.eustace@digital.homeoffice.gov.uk>
2017-10-24 11:37:42 +0200

    testing module for testing account

---
+++ test/main.tf
@@ -0,0 +1,17 @@
+
+resource "random_id" "s3-bucket-name" {
+  byte_length = 10
+}
+
+module "s3-default" {
+    
+    source = "../"
+
+    name                 = "testing-${random_id.s3-bucket-name.hex}"
+    acl                  = "private"
+    environment          = "testing"
+    kms_alias            = "testingkey"
+    bucket_iam_user      = "testing-s3-bucket-user"
+    iam_user_policy_name = "testing-s3-bucket-policy"
+
+}
