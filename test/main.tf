4dd505498d12b86743758ee23cdacb52d29a6079
Ben Eustace <ben.eustace@digital.homeoffice.gov.uk>
2017-10-24 12:18:19 +0200

    testing on count

--- test/main.tf
+++ test/main.tf
@@ -1,18 +1,14 @@
-
 resource "random_id" "s3-bucket-name" {
   byte_length = 10
 }
 
 module "s3-default" {
-    
-    source = "../"
+  source = "../"
 
-    name                 = "testing-${random_id.s3-bucket-name.hex}"
-    acl                  = "private"
-    environment          = "testing"
-    kms_alias            = "testingkey"
-    bucket_iam_user      = "testing-s3-bucket-user"
-    iam_user_policy_name = "testing-s3-bucket-policy"
-
+  name                 = "testing-${random_id.s3-bucket-name.hex}"
+  acl                  = "private"
+  environment          = "testing"
+  kms_alias            = "testingkey"
+  bucket_iam_user      = "testing-s3-bucket-user"
+  iam_user_policy_name = "testing-s3-bucket-policy"
 }
-
