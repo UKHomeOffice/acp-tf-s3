19a04db61430c48b283c3f5e6875108d278821a3
Ben Eustace <ben.eustace@digital.homeoffice.gov.uk>
2017-10-25 10:17:08 +0200

    fix namings of data

--- policy.tf
+++ policy.tf
@@ -140,8 +140,8 @@ data "aws_iam_policy_document" "s3_bucket_with_kms_policy_document" {
   }
 }
 
-data "aws_iam_policy_document" "s3_bucket_policy_document_whitelist" {
-  count = "${var.kms_alias == "" ? 0 : 1 + var.white_list_ip[0] == "*" ? 0 : 2 == 2 ? 1 : 0}"
+data "aws_iam_policy_document" "s3_bucket_policy_document" {
+  count = "${var.kms_alias == "" ? 1 : 0}"
 
   policy_id = "${var.bucket_iam_user}Policy"
 
@@ -206,8 +206,8 @@ data "aws_iam_policy_document" "s3_bucket_policy_document_whitelist" {
   }
 }
 
-data "aws_iam_policy_document" "kms_key_policy_document_whitelist" {
-  count = "${var.kms_alias == "" ? 0 : 1 + var.white_list_ip[0] == "*" ? 0 : 2 == 3 ? 1 : 0}"
+data "aws_iam_policy_document" "kms_key_policy_document" {
+  count = "${var.kms_alias == "" ? 0 : 1}"
 
   policy_id = "${var.kms_alias}Policy"
 
