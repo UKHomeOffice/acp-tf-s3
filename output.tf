db8bc026e75d0590358132a4db6c1fabd02c2820
Jay Keshur <jay.keshur@digital.homeoffice.gov.uk>
2018-04-23 10:10:56 +0100

    Add s3 ARN to output

--- output.tf
+++ output.tf
@@ -3,6 +3,11 @@ output "s3_bucket_id" {
   value       = "${aws_s3_bucket.s3_bucket.id}"
 }
 
+output "s3_bucket_arn" {
+  description = "The S3 bucket ARN we just created"
+  value       = "${aws_s3_bucket.s3_bucket.arn}"
+}
+
 output "s3_bucket_kms_key" {
   description = "The KMS key ID used for the bucket"
   value       = "${aws_kms_key.s3_bucket_kms_key.*.key_id}"
