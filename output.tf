a5f6dbdefd2059287d7a0ec24e309fa7faea19e9
tasharnvb <tasharn.b@gmail.com>
2018-10-02 11:25:54 +0100

    Fix s3 bucket arn output

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "s3_bucket_id" {
-  description = "ID of generated S3 bucket with website hosting enabled"
+  description = "ID of generated S3 bucket"
   value       = "${element(concat(aws_s3_bucket.s3_bucket.*.id, list("")), 0)}"
 }
 
 output "s3_bucket_arn" {
-  description = "ARN of generated S3 bucket with website hosting enabled"
-  value       = "${element(concat(aws_s3_bucket.s3_website_bucket.*.arn, list("")), 0)}"
+  description = "ARN of generated S3 bucket"
+  value       = "${element(concat(aws_s3_bucket.s3_bucket.*.arn, list("")), 0)}"
 }
 
 output "s3_website_bucket_id" {
