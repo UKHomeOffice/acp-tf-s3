8c011d6854f7c5b83d049b1cff7b6052d29c9de2
adekunle ibitayo <adekunle.ibitayo@appvia.io>
2019-06-12 11:08:26 +0100

    adding more kms key outputs

--- output.tf
+++ output.tf
@@ -27,3 +27,33 @@ output "s3_bucket_kms_key_arn" {
   description = "KMS Key ARN of the generated bucket"
   value       = "${aws_kms_key.s3_bucket_kms_key.*.arn}"
 }
+
+output "s3_bucket_whitelist_kms_key" {
+  description = "KMS Key ID of the whitelist ip generated bucket"
+  value       = "${aws_kms_key.s3_bucket_kms_key_whitelist.*.key_id}"
+}
+
+output "s3_bucket_whitelist_arn_kms_key_arn" {
+  description = "KMS Key ARN of the whitelist ip generated bucket"
+  value       = "${aws_kms_key.s3_bucket_kms_key_whitelist.*.arn}"
+}
+
+output "s3_bucket_whitelist_vpc_kms_key" {
+  description = "KMS Key ID of the whitelist vpc generated bucket"
+  value       = "${aws_kms_key.s3_bucket_kms_key_whitelist_vpc.*.key_id}"
+}
+
+output "s3_bucket_whitelist_vpc_arn_kms_key_arn" {
+  description = "KMS Key ARN of the whitelist vpc generated bucket"
+  value       = "${aws_kms_key.s3_bucket_kms_key_whitelist_vpc.*.arn}"
+}
+
+output "s3_bucket_whitelist_ip_and_vpc_kms_key" {
+  description = "KMS Key ID of the whitelist ip and vpc generated bucket"
+  value       = "${aws_kms_key.s3_bucket_kms_key_whitelist_ip_and_vpc.*.key_id}"
+}
+
+output "s3_bucket_whitelist_ip_and_vpc_kms_key_arn" {
+  description = "KMS Key ARN of the whitelist ip and vpc generated bucket"
+  value       = "${aws_kms_key.s3_bucket_kms_key_whitelist_ip_and_vpc.*.arn}"
+}
