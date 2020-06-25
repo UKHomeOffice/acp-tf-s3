1ed0469212bc248a588abb78233faa3441423c2b
Adekunle Ibitayo <adekunle.ibitayo@appvia.io>
2020-06-25 18:05:27 +0100

    adding kms outputs

--- output.tf
+++ output.tf
@@ -17,3 +17,14 @@ output "s3_bucket_kms_key_arn" {
   description = "The KMS key ARN used for the bucket"
   value       = element(concat(aws_kms_key.s3_bucket_kms_key.*.arn, [""]), 0)
 }
+
+output "s3_bucket_whitelist_kms_key" {
+  description = "KMS Key ID of the whitelist ip generated bucket"
+  value       = element(concat(aws_kms_key.s3_bucket_kms_key_whitelist.*.key_id, [""]), 0)
+}
+
+output "s3_bucket_whitelist_arn_kms_key_arn" {
+  description = "KMS Key ARN of the whitelist ip generated bucket"
+  value       = element(concat(aws_kms_key.s3_bucket_kms_key_whitelist.*.arn, [""]), 0)
+}
+
